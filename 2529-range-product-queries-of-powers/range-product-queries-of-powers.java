d973a92683a6cbd89fd03e08fe1555407c5b6e95
PUSHPENDRA-7 <koshtapushpendra320@gmail.com>
2025-08-12 18:43:04 +0530

    Time: 28 ms (27.96%) | Memory: 87.6 MB (46.24%) - LeetSync

---
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] productQueries(int n, int[][] queries) {
+        int Mod = (int) (1e9 + 7);
+        List<Long> power = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+
+        // Build powers array
+        for (int i = 0; i <= 32; i++) {
+            if ((n & (1 << i)) != 0) { // i-th bit is set
+                power.add((long) (1 << i));
+            }
+        }
+
+        for (int[] query : queries) {
+            int start = query[0];
+            int end = query[1];
+
+            long product = 1;
+            for (int i = start; i <= end; i++) {
+                product = (product * power.get(i)) % Mod;
+            }
+
+            result.add((int) product);
+        }
+        // Convert result to int array
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
