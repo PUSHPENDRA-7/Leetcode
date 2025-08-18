f3e8e642a8c0c298150aa84c009431fb30760f57
PUSHPENDRA-7 <koshtapushpendra320@gmail.com>
2025-08-18 22:02:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (26.20%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double sum = Math.pow(x,n);
+    //    for(int i=1; i<n;i++){
+    //         sum = x*n;
+    //    }
+       return sum; 
+    }
+}
