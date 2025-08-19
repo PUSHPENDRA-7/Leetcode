4c4d64f49beae7b5cba3dca95f89eb4b7b4e2ba1
PUSHPENDRA-7 <koshtapushpendra320@gmail.com>
2025-08-19 20:12:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (20.32%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,9 +1,16 @@
 class Solution {
+    public double solve(double x, long n) {
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 1.0 / solve(x, -n);
+        if (n % 2 == 0)
+            return solve(x * x, n / 2);
+
+        return x * solve(x * x, (n - 1) / 2);
+    }
+
     public double myPow(double x, int n) {
-        double sum = Math.pow(x,n);
-    //    for(int i=1; i<n;i++){
-    //         sum = x*n;
-    //    }
-       return sum; 
+        return solve(x, (long) n);
     }
 }
