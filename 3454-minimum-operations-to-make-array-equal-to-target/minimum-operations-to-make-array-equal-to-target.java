465087bb58ff46791072701de73b39b36e4b8d7a
PUSHPENDRA-7 <koshtapushpendra320@gmail.com>
2025-10-30 20:10:41 +0530

    Time: 3 ms (70.31%) | Memory: 58.8 MB (58.44%) - LeetSync

---
+++ 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long minimumOperations(int[] nums, int[] target) {
+        int n = nums.length;
+        long result = 0;
+        int curr = 0;
+        int prev = 0;
+
+        for (int i = 0; i < n; i++) {
+            curr = target[i] - nums[i];
+
+            //check sign
+
+            if ((curr > 0 && prev < 0) || (curr < 0 && prev > 0)) {
+                result += Math.abs(curr);
+            } else if (Math.abs(curr) > Math.abs(prev)) {
+                result += Math.abs(curr - prev);
+            }
+
+            prev = curr;
+        }
+
+        return result;
+
+    }
+}
