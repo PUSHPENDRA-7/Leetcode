f92411b7e4eeaa67522fc1574fa13abe83c25340
PUSHPENDRA-7 <koshtapushpendra320@gmail.com>
2025-10-31 21:05:04 +0530

    Time: 1 ms (100.00%) | Memory: 45.5 MB (7.71%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,21 +1,34 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> mp = new HashMap<>();
+        // List<Integer> result = new ArrayList<>();
+        // Map<Integer, Integer> mp = new HashMap<>();
 
-        for (int num : nums) {
-            mp.put(num, mp.getOrDefault(num, 0) + 1);
-            if (mp.get(num) == 2) {
-                result.add(num);
+        // for (int num : nums) {
+        //     mp.put(num, mp.getOrDefault(num, 0) + 1);
+        //     if (mp.get(num) == 2) {
+        //         result.add(num);
+        //     }
+
+        // }
+        // //Convert List<Integer> to int[]
+        // int[] res = new int[result.size()];
+        // for (int i = 0; i < result.size(); i++) {
+        //     res[i] = result.get(i);
+        // }
+
+        // return res;
+
+        int[] result = new int[2];
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    result[count] = nums[i];
+                    count++;
+                    break;
+                }
             }
-
         }
-        //Convert List<Integer> to int[]
-        int[] res = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            res[i] = result.get(i);
-        }
-
-        return res;
+        return result;
     }
 }
