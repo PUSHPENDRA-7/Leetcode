581dc802c8221f51915e827d99bb8016652cd4e1
PUSHPENDRA-7 <koshtapushpendra320@gmail.com>
2025-11-10 21:12:09 +0530

    Time: 143 ms (38.03%) | Memory: 123.5 MB (12.68%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
@@ -1,41 +1,41 @@
-// class Solution {
-//     public int minOperations(int[] nums) {
-//         // monotonic - increasing stack
-//         // T.C O(n)
-//         // S.C O(n)
-//         Stack<Integer> st = new Stack<>();
-//         int ops = 0;
-
-//         for (int num : nums) {
-//             while (!st.isEmpty() && st.peek() > num) {
-//                 st.pop();
-//             }
-
-//             if (num == 0)
-//                 continue;
-
-//             if (st.isEmpty() || st.peek() < num) {
-//                 ops++;
-//                 st.push(num);
-//             }
-//         }
-//         return ops;
-//     }
-// }
-
 class Solution {
     public int minOperations(int[] nums) {
-        var stack = new int[nums.length + 1];
-        var top = 0;
-        var ans = 0;
-        for (var i = 0; i < nums.length; i++) {
-            while (stack[top] > nums[i]) {
-                top--;
-                ans++;
+        // monotonic - increasing stack
+        // T.C O(n)
+        // S.C O(n)
+        Stack<Integer> st = new Stack<>();
+        int ops = 0;
+
+        for (int num : nums) {
+            while (!st.isEmpty() && st.peek() > num) {
+                st.pop();
             }
-            if (stack[top] != nums[i])
-                stack[++top] = nums[i];
+
+            if (num == 0)
+                continue;
+
+            if (st.isEmpty() || st.peek() < num) {
+                ops++;
+                st.push(num);
+            }
         }
-        return ans + top;
+        return ops;
     }
 }
+
+// class Solution {
+//     public int minOperations(int[] nums) {
+//         var stack = new int[nums.length + 1];
+//         var top = 0;
+//         var ans = 0;
+//         for (var i = 0; i < nums.length; i++) {
+//             while (stack[top] > nums[i]) {
+//                 top--;
+//                 ans++;
+//             }
+//             if (stack[top] != nums[i])
+//                 stack[++top] = nums[i];
+//         }
+//         return ans + top;
+//     }
+// }
