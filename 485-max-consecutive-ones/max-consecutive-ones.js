8639f7db19f569ca6599e2584f474754b90511f1
PUSHPENDRA-7 <koshtapushpendra320@gmail.com>
2025-11-12 00:18:22 +0530

    Time: 2 ms (70.63%) | Memory: 60.7 MB (32.08%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -2,15 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxConsecutiveOnes = function(nums) {
+var findMaxConsecutiveOnes = function (nums) {
     let cnt = 0;
     let max = 0;
 
-    for(let i=0; i<nums.length; i++){
-        if(nums[i] == 1){
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
             cnt++;
-        }else{
-            cnt =0;
+        } else {
+            cnt = 0;
         }
         max = Math.max(max, cnt);
     }
